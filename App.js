d6dfc004d9b5a2918ae2e46b1b9dd3ea2084f6c1
alickcai <caizhenqi@sina.com>
2023-05-13 21:33:48 +0800

    202305132133

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+
+// To anybody from Expo:
+// This is a serious issue impacting production release
+// Pls stop Expo Bot to close issue unreasonably, which's destroying Expo credit
+
+function App() {
+  return (
+    <View Style={{ flex: 1 }}>
+      <Text>This is Expo Crash issue at starting stage...</Text>
+    </View>
+  );
+}
+
+export default App;
